8b1d90fcd6733695211368b8237f96c3970b47ac
devDaviSousa <devsousadavi@gmail.com>
2021-12-23 17:58:57 -0300

    Finanlizando a home page usando next

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { SindInButton } from '../SignInButton'
+import styles from './styles.module.scss'
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ignews" />
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a >Posts</a>
+        </nav>
+        <SindInButton />
+      </div>
+    </header>
+  )
+}
