6cdfcd9d4f3fca73dae224e54e9a972e57a65642
devDaviSousa <devsousadavi@gmail.com>
2022-03-02 21:59:35 -0300

    usando o next-auth para fazer autenticação com o github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,29 @@
-import { FaGithub } from 'react-icons/fa'
-import { FiX } from 'react-icons/fi'
-import styles from './styles.module.scss'
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import styles from "./styles.module.scss";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export function SindInButton() {
-  const isUserLoggerdIn = true;
+  const { data: session } = useSession();
 
-  return isUserLoggerdIn ? (
-    <button type="button"
-      className={styles.signInButton}>
-      <FaGithub color='#04d361' />
-      Davi Sousa
-      <FiX color='#737380' className={styles.closeIcon} />
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
+      <FaGithub color="#04d361" />
+      {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} />
     </button>
-
   ) : (
-    <button type="button"
-      className={styles.signInButton}>
-      <FaGithub color='#eba417' />
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
+      <FaGithub color="#eba417" />
       Sign in with github
-    </button >
-
+    </button>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,15 @@
+import { AppProps } from "next/app";
+import { Header } from "../components/Header";
+import { SessionProvider } from "next-auth/react";
 
-import { AppProps } from 'next/app'
-import { Header } from '../components/Header'
-
-import '../styles/global.scss'
-function MyApp({ Component, pageProps }: AppProps) {
+import "../styles/global.scss";
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <>
+    <SessionProvider session={session}>
       <Header />
       <Component {...pageProps} />
-    </>
-  )
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
