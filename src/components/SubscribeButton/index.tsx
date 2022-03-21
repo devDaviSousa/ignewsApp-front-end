97ae71a8be51bade3443277b23c46fb1a6c832c0
devDaviSousa <devsousadavi@gmail.com>
2022-03-21 07:53:16 -0300

    checkout stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,16 +1,28 @@
-import styles from './styles.module.scss'
+import { useSession, signIn } from "next-auth/react";
+import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
   priceId: string;
-
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+    //criação da checkout session https://stripe.com/docs/api/checkout/sessions
+  }
 
   return (
     <button
       type="button"
       className={styles.subscribeButton}
-    >Subscribe now</button>
-  )
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  );
 }
